7774cdc78dc7de7cd0de45f19caa1bfcbafcceb2
hari715om <singh.omhari715@gmail.com>
2025-11-28 12:36:24 +0530

    ml first lab program

---
+++ lab1/ml_lab1.py
@@ -0,0 +1,37 @@
+import csv
+
+data = [
+    {'name': 'Nikhil', 'branch': 'COE', 'year': 2, 'cgpa': 9.0},
+    {'name': 'Sanchit', 'branch': 'COE', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Aditya', 'branch': 'IT', 'year': 2, 'cgpa': 9.3},
+    {'name': 'Sagar', 'branch': 'SE', 'year': 1, 'cgpa': 9.5},
+    {'name': 'Prateek', 'branch': 'MCE', 'year': 3, 'cgpa': 7.8},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1},
+    {'name': 'Sahil', 'branch': 'EP', 'year': 2, 'cgpa': 9.1}
+]
+
+with open('university_records.csv', 'w', newline='') as csvfile:
+    fieldnames = ['name', 'branch', 'year', 'cgpa']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(data)
+
+    import csv
+with open('university_records.csv', mode ='r') as file:    
+       csvFile = csv.DictReader(file)
+       for lines in csvFile:
+            print(lines)
